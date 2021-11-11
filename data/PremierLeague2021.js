f63f1e680af0ebf7e9688f39b3f1388176c1e868
유민호 <wingedcompany@gmail.com>
2021-11-12 02:33:51 +0900

    commit 8

--- data/PremierLeague2021.js
+++ data/PremierLeague2021.js
@@ -2,102 +2,102 @@ export default [
     {
         "name": "MCI",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t43.png",
-        "color": "#253541",
-        "values": [3, 3, 4, 7, 8, 11, 12, 12, 15, 18, 19, 20, 23, 26, 26, 29, 32, 38, 41, 44, 47, 50, 56, 59, 65, 68, 71, 71, 74, 74, 77, 77, 80, 80, 83, 83, 86]
+        "color": "#ABDEF9",
+        "values": [0, 3, 3, 4, 7, 8, 11, 12, 12, 15, 18, 19, 20, 23, 26, 26, 29, 32, 38, 41, 44, 47, 50, 56, 59, 65, 68, 71, 71, 74, 74, 77, 77, 80, 80, 83, 83, 86]
     }, {
         "name": "MUN",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t1.png",
-        "color": "#89a6e4",
-        "values": [0, 3, 3, 6, 7, 7, 10, 13, 16, 19, 20, 23, 26, 27, 30, 33, 36, 40, 40, 41, 44, 45, 46, 49, 51, 54, 57, 57, 60, 63, 66, 67, 67, 70, 70, 71, 74]
+        "color": "#FF2A12",
+        "values": [0, 0, 3, 3, 6, 7, 7, 10, 13, 16, 19, 20, 23, 26, 27, 30, 33, 36, 40, 40, 41, 44, 45, 46, 49, 51, 54, 57, 57, 60, 63, 66, 67, 67, 70, 70, 71, 74]
     }, {
         "name": "ARS",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t3.png",
-        "color": "#93e893",
+        "color": "#DE0003",
         "values": [3, 6, 6, 9, 9, 9, 12, 12, 13, 13, 13, 13, 14, 14, 17, 20, 23, 24, 27, 30, 31, 31, 31, 34, 34, 37, 38, 41, 42, 42, 45, 46, 46, 49, 55, 55, 58, 61]
     }, {
         "name": "LEI",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t13.png",
-        "color": "#97ac86",
+        "color": "#0B2694",
         "values": [3, 6, 9, 9, 9, 12, 15, 18, 18, 18, 21, 24, 24, 27, 28, 29, 32, 32, 38, 39, 39, 42, 43, 46, 49, 50, 53, 56, 56, 56, 56, 59, 62, 63, 66, 66, 66, 66]
     }, {
         "name": "CHE",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t8.png",
-        "color": "#49c762",
+        "color": "#003E90",
         "values": [3, 3, 4, 7, 8, 9, 12, 15, 18, 19, 22, 22, 22, 25, 25, 26, 26, 26, 29, 30, 33, 36, 39, 42, 43, 47, 50, 51, 51, 51, 54, 55, 58, 61, 64, 64, 67, 67]
     }, {
         "name": "NEW",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t4.png",
-        "color": "#4509a5",
+        "color": "#000000",
         "values": [3, 3, 4, 7, 7, 8, 11, 11, 11, 14, 14, 17, 17, 18, 18, 19, 19, 19, 19, 19, 22, 22, 25, 25, 25, 26, 27, 28, 28, 29, 32, 35, 36, 36, 39, 39, 42, 45]
     }, {
         "name": "WOL",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t39.png",
-        "color": "#fd28ad",
+        "color": "#FFBA1A",
         "values": [3, 3, 3, 6, 9, 10, 13, 13, 14, 17, 17, 17, 20, 20, 21, 21, 22, 22, 22, 23, 23, 26, 27, 30, 33, 34, 35, 35, 35, 35, 38, 41, 41, 42, 45, 45, 45, 45]
     }, {
         "name": "LIV",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t14.png",
-        "color": "#1aa058",
+        "color": "#D85252",
         "values": [3, 6, 9, 9, 10, 13, 16, 17, 20, 21, 24, 25, 28, 31, 32, 33, 33, 33, 34, 37, 40, 40, 40, 40, 40, 43, 43, 46, 46, 49, 52, 53, 54, 54, 60, 63, 66, 69]
     }, {
         "name": "CRY",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t31.png",
-        "color": "#336fe9",
+        "color": "#1E4A92",
         "values": [3, 6, 6, 6, 7, 10, 10, 13, 13, 13, 16, 17, 18, 18, 18, 19, 22, 23, 23, 23, 26, 29, 29, 29, 32, 34, 34, 37, 37, 38, 38, 38, 38, 38, 41, 44, 44, 44]
     }, {
         "name": "EVE",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t11.png",
-        "color": "#f64a1b",
+        "color": "#1E4A92",
         "values": [3, 6, 9, 12, 13, 13, 13, 13, 16, 16, 17, 20, 23, 26, 29, 29, 29, 32, 32, 33, 33, 36, 37, 37, 40, 46, 46, 46, 46, 47, 48, 49, 52, 52, 56, 56, 59, 59]
     }, {
         "name": "LEE",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t2.png",
-        "color": "#a9f8ec",
+        "color": "#FFFF00",
         "values": [0, 3, 6, 7, 7, 10, 10, 10, 11, 14, 14, 14, 17, 17, 20, 23, 23, 23, 23, 26, 29, 29, 32, 32, 35, 35, 35, 36, 39, 42, 45, 46, 47, 47, 50, 53, 56, 59]
     }, {
         "name": "SOU",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t20.png",
-        "color": "#27e123",
+        "color": "#146B2B",
         "values": [0, 3, 6, 7, 7, 10, 10, 10, 11, 14, 14, 14, 17, 17, 20, 23, 23, 23, 23, 26, 29, 29, 32, 32, 35, 35, 35, 36, 39, 42, 45, 46, 47, 47, 50, 53, 56, 59]
     }, {
         "name": "TOT",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t6.png",
-        "color": "#b1a9a0",
+        "color": "#001757",
         "values": [0, 3, 4, 7, 8, 11, 14, 17, 20, 21, 24, 25, 25, 25, 26, 26, 29, 30, 33, 33, 33, 33, 36, 36, 36, 42, 45, 45, 48, 49, 49, 53, 53, 56, 56, 59, 59, 62]
     }, {
         "name": "BHA",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t36.png",
-        "color": "#30590f",
+        "color": "#0054A6",
         "values": [0, 3, 3, 3, 4, 5, 5, 6, 9, 10, 10, 10, 11, 12, 13, 13, 14, 14, 17, 18, 21, 24, 25, 26, 26, 26, 26, 29, 32, 32, 33, 34, 34, 37, 37, 38, 41, 41]
     }, {
         "name": "SHU",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t49.png",
-        "color": "#beb990",
+        "color": "#1D1617",
         "values": [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 5, 5, 8, 8, 11, 11, 11, 11, 14, 14, 14, 14, 14, 14, 14, 17, 17, 17, 20, 20, 23]
     }, {
         "name": "WHU",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t21.png",
-        "color": "#a8f948",
+        "color": "#7B2539",
         "values": [0, 0, 3, 6, 7, 8, 8, 11, 14, 17, 17, 20, 21, 21, 22, 23, 26, 26, 32, 35, 35, 38, 39, 42, 45, 45, 48, 48, 49, 52, 55, 55, 55, 58, 58, 59, 62, 65]
     }, {
         "name": "FUL",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t54.png",
-        "color": "#dc429b",
+        "color": "#1D1617",
         "values": [0, 0, 0, 0, 1, 1, 4, 4, 4, 7, 7, 8, 9, 10, 11, 11, 11, 12, 12, 13, 14, 14, 15, 19, 22, 23, 26, 26, 26, 26, 26, 27, 27, 27, 27, 27, 28, 28]
     }, {
         "name": "WBA",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t35.png",
-        "color": "#832cf",
+        "color": "#181C58",
         "values": [0, 0, 1, 1, 2, 3, 3, 3, 3, 6, 6, 6, 7, 7, 8, 8, 8, 8, 11, 11, 12, 12, 12, 13, 14, 17, 18, 18, 18, 21, 24, 24, 25, 26, 26, 26, 26, 26]
     }, {
         "name": "AVL",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t7.png",
-        "color": "#37ef99",
-        "values": [3, 6, 9, 12, 12, 12, 15, 15, 15, 15, 18, 19, 22, 25, 26, 26, 26, 29, 29, 32, 32, 35, 36, 36, 39, 40, 41, 41, 44, 44, 44, 45, 48, 49, 49, 52, 55]
+        "color": "#FFED00",
+        "values": [0, 3, 6, 9, 12, 12, 12, 15, 15, 15, 15, 18, 19, 22, 25, 26, 26, 26, 29, 29, 32, 32, 35, 36, 36, 39, 40, 41, 41, 44, 44, 44, 45, 48, 49, 49, 52, 55]
     }, {
         "name": "BUR",
         "src": "https://resources.premierleague.com/premierleague/badges/25/t90.png",
-        "color": "#29ee08",
-        "values": [0, 0, 0, 1, 1, 1, 2, 5, 5, 6, 9, 10, 13, 13, 16, 16, 16, 19, 22, 22, 22, 23, 27, 28, 29, 30, 33, 33, 33, 33, 33, 36, 36, 39, 39, 39, 39]
+        "color": "#8CD1F3",
+        "values": [0, 0, 0, 0, 1, 1, 1, 2, 5, 5, 6, 9, 10, 13, 13, 16, 16, 16, 19, 22, 22, 22, 23, 27, 28, 29, 30, 33, 33, 33, 33, 33, 36, 36, 39, 39, 39, 39]
     }
 ];
